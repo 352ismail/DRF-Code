011ac0bd60401be9e5ef5312c377f5f632b96d0f
Ismail Khan <352ismailkhan@gmail.com>
2023-09-27 18:23:22 +0500

    Custom_auth

--- pyclient/create.py
+++ pyclient/create.py
@@ -3,12 +3,15 @@ import requests
 # for i in range(50,100,2):
 endpoint = "http://127.0.0.1:8000/api/products/"
 
+headers = { "Authorization": "Bearer f55b0d7c7f4c335aa4b157d60c38838bc0146a84"}
+
+
 data = {
         "title":f"Gifting Items",
         "content":"Packings of gifts for all type of itmes",
         "price": 1200
 }
-response = requests.post(endpoint, json=data)
+response = requests.post(endpoint, json=data, headers=headers)
 if response.status_code == 200:
     print(response.json())
     #print(response.status_code)
--- pyclient/list.py
+++ pyclient/list.py
@@ -1,10 +1,38 @@
 import requests
+from  getpass import getpass
 
-endpoint = "http://127.0.0.1:8000/api/products/"
-response = requests.get(endpoint)
-if response.status_code == 200:
-    print(response.json())
+#get token 
+auth_endpoint = "http://127.0.0.1:8000/api/auth/"
+
+
+username = input("Enter your username: ")
+password = getpass(prompt="Enter your password: ")
+#your credentials
+data ={
+    "username":username,
+    "password":password
+}
+auth_response = requests.post(auth_endpoint, json=data)
+if auth_response.status_code == 200:
+
+    token = auth_response.json()["token"]
+    print(token)
+
+    headers = {"Authorization":f"Bearer {token}"}
+        # Send the token in request headers 
+    endpoint = "http://127.0.0.1:8000/api/products/"
+    response = requests.get(endpoint, headers=headers)
+    if response.status_code == 200:
+        print(response.json())
+        #print(response.status_code)
+    else:
+        print(response.json())
+        print(response.status_code) 
+    print(auth_response.json())
     #print(response.status_code)
 else:
-    print(response)
-    print(response.status_code) 
+    print(auth_response.json())
+    print(auth_response.status_code) 
+
+
+
